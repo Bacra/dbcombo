1a3cfe56042d84188932ed2817dfebc683668eac
Bacra <bacra.woo@gmail.com>
2016-11-03 11:14:31 +0800

    Fix: ie6 7 8 width assert

--- test/test_stringify.js
+++ test/test_stringify.js
@@ -1,4 +1,4 @@
-var assert		= require('assert');
+var expect		= require('expect.js');
 var stringify	= require('../').stringify;
 var DEF			= require('../lib/def');
 
@@ -6,18 +6,18 @@ describe('#stringify', function()
 {
 	it('#base', function()
 	{
-		assert.equal(stringify([0]), 'Y1');
-		assert.equal(stringify([1]), 'Y2');
-		assert.equal(stringify([0,1]), 'Y3');
-		assert.equal(stringify([0, 30]), '1000001');
-		assert.equal(stringify([0, 31]), 'Y1Y1');
-		assert.equal(stringify([0, 31, 31]), 'Y1Y1');
-		assert.equal(stringify([0, 31, 92, 93, 94]), 'Y31000000Y1Y1');
-		assert.equal(stringify([0, 93, 92, 31, 94]), 'Y31000000Y1Y1');
-		assert.equal(stringify([1000]), 'Y80W32X');
-		assert.equal(stringify([2000]), 'Y2000W29X/W35X');
-		assert.equal(stringify([5000]), 'Yg0W21X/W35X/W35X/W35X/W35X');
-		assert.equal(stringify([4000, 34]), 'Y2W24X/W35X/W35X/W33XY8Z');
+		expect(stringify([0])).to.be('Y1');
+		expect(stringify([1])).to.be('Y2');
+		expect(stringify([0,1])).to.be('Y3');
+		expect(stringify([0, 30])).to.be('1000001');
+		expect(stringify([0, 31])).to.be('Y1Y1');
+		expect(stringify([0, 31, 31])).to.be('Y1Y1');
+		expect(stringify([0, 31, 92, 93, 94])).to.be('Y31000000Y1Y1');
+		expect(stringify([0, 93, 92, 31, 94])).to.be('Y31000000Y1Y1');
+		expect(stringify([1000])).to.be('Y80W32X');
+		expect(stringify([2000])).to.be('Y2000W29X/W35X');
+		expect(stringify([5000])).to.be('Yg0W21X/W35X/W35X/W35X/W35X');
+		expect(stringify([4000, 34])).to.be('Y2W24X/W35X/W35X/W33XY8Z');
 	});
 
 
@@ -31,7 +31,7 @@ describe('#stringify', function()
 		}
 
 		var key = stringify(arr);
-		assert.equal(key, 'Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1/Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1/Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1');
+		expect(key).to.be('Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1/Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1/Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1Y1');
 	});
 
 
@@ -46,6 +46,6 @@ describe('#stringify', function()
 
 		var key = stringify(arr);
 
-		assert.equal(key, 'Y40Y20/Y10YgY8Y4Y2Y1Z1000000Yg00000Y800000Y400000Y200000Y100000Yg0000Y80000Y40000Y20000Y10000Yg000Y8000Y4000Y2000Y1000Yg00Y800Y400Y200Y100Yg0Y80Y40Y20Y10YgY8/Y4Y2Y1Z1000000Yg00000Y800000Y400000Y200000Y100000Yg0000Y80000Y40000Y20000Y10000Yg000Y8000Y4000Y2000Y1000Yg00Y800Y400Y200Y100Yg0Y80Y40Y20Y10YgY8Y4Y2Y1');
+		expect(key).to.be('Y40Y20/Y10YgY8Y4Y2Y1Z1000000Yg00000Y800000Y400000Y200000Y100000Yg0000Y80000Y40000Y20000Y10000Yg000Y8000Y4000Y2000Y1000Yg00Y800Y400Y200Y100Yg0Y80Y40Y20Y10YgY8/Y4Y2Y1Z1000000Yg00000Y800000Y400000Y200000Y100000Yg0000Y80000Y40000Y20000Y10000Yg000Y8000Y4000Y2000Y1000Yg00Y800Y400Y200Y100Yg0Y80Y40Y20Y10YgY8Y4Y2Y1');
 	});
 });

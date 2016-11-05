9ae3846cf7801ebefbe2034aae7f45e4e4e78bf6
Bacra Woo <bacra.woo@gmail.com>
2016-11-05 12:30:49 +0800

    Add: test-e2e-dev script

--- karma.conf.js
+++ karma.conf.js
@@ -3,12 +3,13 @@ var extend = require('extend');
 var baseConfig = require('./test/build/karma.base.conf.js');
 var osConfig = require('./test/build/karma.os.conf.js');
 var sauceConfig = require('./test/build/karma.sauce.conf.js');
+var browsers = require('./test/build/sl_browsers.js');
 
 module.exports = function(config)
 {
 	var key = process.argv[4];
 	var base = baseConfig(config);
-	var custom = key ? sauceConfig(config) : osConfig(config);
+	var custom = browsers.groups[key] ? sauceConfig(config) : osConfig(config);
 
 	config.set(extend({}, base, custom));
 };

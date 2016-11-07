46b77807c44a719a0171f1d32e3a23c439735ecc
Bacra Woo <bacra.woo@gmail.com>
2016-11-08 00:18:28 +0800

    Add: delay test

---
+++ test/browser/source/seajs-plugin-src/db.js/Yf01.js
@@ -0,0 +1,5 @@
+define('a0', [], function(require, exports){exports.a0 = true});
+define('a10', [], function(require, exports){exports.a10 = true});
+define('a11', [], function(require, exports){exports.a11 = true});
+define('a12', [], function(require, exports){exports.a12 = true});
+define('a13', [], function(require, exports){exports.a13 = true});

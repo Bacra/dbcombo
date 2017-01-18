d82ed7d6422e392614ac94e48a9624967977e9a9
Bacra <bacra.woo@gmail.com>
2017-01-18 15:27:54 +0800

    Clear

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 var extend = require('extend');
 
-var custom = {
-	resolve: {
+var custom =
+{
+	resolve:
+	{
 		// root:
 		modulesDirectories: [__dirname+'/node_modules']
 	},
-	entry: {
+	entry:
+	{
 		// webpack bugs
 		// https://github.com/webpack/webpack/issues/300
 		// 必须设置成数组，否则，require entry中定义的入口，是会跑错的
 		'seajs-plugin': ['./src/seajs-plugin.js'],
 		'benchmark4browser': './benchmark/benchmark.js'
 	},
-	output: {
+	output:
+	{
 		path: 'dist',
 		filename: '[name].js'
 	}

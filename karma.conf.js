305c9e320bb808b03c49cecd7d400c12089de95f
Bacra Woo <bacra.woo@gmail.com>
2018-07-03 12:53:28 +0800

    Fix: sauce config

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config)
 	if (browsers.groups[key])
 		custom = sauceConfig(config, browsers.groups[key]);
 	else if (browsers.list[key])
-		custom = sauceConfig(config, [browsers.list[key]]);
+		custom = sauceConfig(config, [key]);
 	else if (!key)
 		custom = osConfig(config);
 	else

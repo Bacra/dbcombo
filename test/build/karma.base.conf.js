2ba8044755772d58c698a0de06df4226a0c4138c
Bacra Woo <bacra.woo@gmail.com>
2018-07-03 12:33:21 +0800

    Update: Chrome_travis_ci

--- test/build/karma.base.conf.js
+++ test/build/karma.base.conf.js
@@ -74,7 +74,7 @@ module.exports = function(config)
 			Chrome_travis_ci:
 			{
 				base: 'Chrome',
-				flags: ['--no-sandbox']
+				flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
 			}
 		},
 	};
